1f10c6e05d60a1b6a267199f8c7f1a5d121ebf3b
arahna41 <arahna41@gmail.com>
2022-08-09 15:45:01 +0300

    add jquery code

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-import "jquery";
+import $ from "jquery";
+
+$(document).ready(function () {
+  $(".list-item:first").hover(function () {
+    $(this).toggleClass("active");
+  });
+
+  $(".list-item:eq(2)").on("click", () => {
+    $(".image:even").fadeToggle("slow");
+  });
+
+  $(".list-item:eq(4)").on("click", () => {
+    $(".image:odd").animate(
+      {
+        opacity: "toggle",
+        height: "toggle",
+      },
+      2000
+    );
+  });
+});
